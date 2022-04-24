98f46e5d5aa9454db5309f3c7f98a02d2d6ee852
Eduardo Nagashima <eduyudji@gmail.com>
2022-04-24 11:22:15 -0300

    projeto criado

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const usuarios = [{
+	username: 'bobesponja', 
+	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+}];
+
+const tweets = [{
+	username: "bobesponja",
+	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    tweet: "eu amo o hub",
+}];
+
+app.post('/sign-up', (req, res)=>{
+    const usuario = req.body;
+    usuarios.push(usuario);
+    res.send("OK");
+});
+
+app.post('/tweets', (req,res)=>{
+    const tweet = req.body;
+    tweets.push(tweet);
+    res.send("OK");
+});
+
+app.get('/tweets', (req,res)=>{
+    res.send(tweets);
+})
+
+app.listen(5000);
